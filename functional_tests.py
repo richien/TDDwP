51cd9d0450952641b3ceb803e2b6491ccd739c5b
Geoffrey Willis <benwill2014@gmail.com>
2018-12-08 23:15:43 +0300

    User can input a list item and it  gets displayed in the home page

--- functional_tests.py
+++ functional_tests.py
@@ -32,12 +32,12 @@ class NewVisitorTest(unittest.TestCase):
 		# "1: Buy peacock feathers" as an item in a to-do list
 		inputbox.send_keys(Keys.ENTER)
 
+		# import time
+		# time.sleep(10)
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows),
-			"New to-do item did not appear in table"
-		)
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 		# There is still a text box inviting him to add another item. He 
 		# enters "Use peacock feathers to make a fly" (He is very methodical)
 		self.fail('Finish the test!')
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def home_page(request):
-	return render(request, 'lists/home.html')
+	# if request.method == 'POST':
+	# 	return HttpResponse(request.POST['item_text'])
+	return render(request, 'lists/home.html', {'new_item_text' : request.POST.get('item_text', ''),})
